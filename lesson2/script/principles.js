2f394b4991a5fd81031b1cda99b7571cf8a024a7
BenjaminCDye <dye19007@byui.edu>
2022-09-24 20:06:06 -0600

    principals

---
+++ lesson2/script/principles.js
@@ -0,0 +1,10 @@
+const today = new Date();
+// const makes things faster and easier for memory, use whenever possible.
+document.querySelector("footer div span").textContent = today.getFullYear();
+document.getElementById("lastUpdateDate").textContent = document.lastModified;
+// const lastModified = new Date(document.lastModified);
+// document.getElementById(
+//   "lastUpdateDate"
+// ).textContent = `${lastModified.getDate()}/${
+//   lastModified.getMonth() + 1
+// }/${lastModified.getFullYear()}`;
