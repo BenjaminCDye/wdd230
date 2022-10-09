e5a751d9e3133769c5f6cb6fbcafde4371b8b672
BenjaminCDye <dye19007@byui.edu>
2022-10-08 23:04:00 -0600

    pge

---
+++ chamber/js/script.js
@@ -0,0 +1,27 @@
+const datefield = document.querySelector("#date");
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+	now
+);
+datefield.innerHTML = `<em>${fulldate}</em>`;
+
+function openNav() {
+    document.getElementById("hamburgerIcon")
+}
+
+
+
+
+
+
+
+const today = new Date();
+// const makes things faster and easier for memory, use whenever possible.
+document.querySelector("footer div span").textContent = today.getFullYear();
+// document.getElementById("lastUpdateDate").textContent = document.lastModified;
+const lastModified = new Date(document.lastModified);
+document.getElementById(
+  "lastUpdateDate"
+).textContent = `${lastModified.getDate()}/${
+  lastModified.getMonth() + 1
+}/${lastModified.getFullYear()}`;
