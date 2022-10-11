753b610a9b1edb657313b747a80ae6c2ca18c3db
BenjaminCDye <dye19007@byui.edu>
2022-10-11 14:40:24 -0600

    script

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -18,10 +18,10 @@ function openNav() {
 const today = new Date();
 // const makes things faster and easier for memory, use whenever possible.
 document.querySelector("footer div span").textContent = today.getFullYear();
-// document.getElementById("lastUpdateDate").textContent = document.lastModified;
-const lastModified = new Date(document.lastModified);
-document.getElementById(
-  "lastUpdateDate"
-).textContent = `${lastModified.getDate()}/${
-  lastModified.getMonth() + 1
-}/${lastModified.getFullYear()}`;
+document.getElementById("lastUpdateDate").textContent = document.lastModified;
+// const lastModified = new Date(document.lastModified);
+// document.getElementById(
+//   "lastUpdateDate"
+// ).textContent = `${lastModified.getDate()}/${
+//   lastModified.getMonth() + 1
+// }/${lastModified.getFullYear()}`;
